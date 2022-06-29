e31d323d174ed2d08aa3aeb29a89d4ba7ef5d309
Richard Karp <richard.karp@lightspeedhq.com>
2022-06-29 11:02:10 -0400

    updates the form to send days, hours and minutes instead of a date

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,6 @@ import {
   Box,
   Button,
 	Header,
-	DatePicker,
   FieldLabel,
   NumberInput
 } from '@lightspeed/design-system-react';
@@ -13,15 +12,15 @@ import {
 export const Form = () => {
 	const { control, handleSubmit } = useForm({
 		defaultValues: {
-      discounts: [{ date: new Date(), reduction: 0 }]
+      discounts: [{ days: 0, hours: 0, minutes: 0, reduction: 0,}]
     }
 	});
 	const { fields, append, remove } = useFieldArray({
     control,
     name: "discounts"
   });
-	const onSubmit = (formData, event) => {
-    event?.preventDefault();
+	const onSubmit = (event, formData) => {
+		event.preventDefault();
     console.log(formData)
   };
 	return(
@@ -35,48 +34,115 @@ export const Form = () => {
 				{fields.map(({ id }, index) => {
 					return (
 						<div key={id}>
-							<Box py={4} className="vd-flex vd-flex--justify-between vd-flex--align-end">
-							<FieldLabel label="Date:">
+							<Box py={4} className="vd-flex vd-flex--justify-between vd-flex--align-end"
+								css={{
+                  '& label': {
+										maxWidth: '100px'
+                  },
+                }}
+							>
+							<FieldLabel label="Days:">
 								<Controller
-									name={`discounts[${index}].date`}
+									name={`discounts[${index}].days`}
 									control={control}
 									render={({ field }) => (
-										<DatePicker
-											date={field.value}
-											defaultValue={new Date()}
+										<NumberInput
+											autoFocus
+											errorMessages={{
+												max: 'Amount cannot exceed 100',
+												min: 'Amount cannot be below 0',
+												nonDecimal: 'Amount can’t contain a decimal point',
+												nonNegative: 'Amount can’t be negative',
+											}}
+											validators={{
+												max: true,
+												min: true,
+												nonDecimal: true,
+												nonNegative: true,
+											}}
+											min={0}
 											{...field}
 										/>
 									)}
 								/>
 							</FieldLabel>
-							<FieldLabel label="Reduction">
-							<Controller
-            		name={`discounts[${index}].reduction`}
-            		control={control}
-            		render={({ field }) => (
-									<NumberInput
-										autoFocus
-										errorMessages={{
-											max: 'Amount cannot exceed 100',
-											min: 'Amount cannot be below 0',
-											nonDecimal: 'Amount can’t contain a decimal point',
-											nonEmpty: 'Enter amount',
-											nonNegative: 'Amount can’t be negative',
-											nonZero: 'Amount must be greater than 0'
-										}}
-										validators={{
-											max: true,
-											min: true,
-											nonDecimal: true,
-											nonEmpty: true,
-											nonNegative: true,
-										}}
-										min={0}
-										max={100}
-										{...field}
-									/>
-								)}
-							/>
+							<FieldLabel label="Hours:">
+								<Controller
+									name={`discounts[${index}].hours`}
+									control={control}
+									render={({ field }) => (
+										<NumberInput
+											autoFocus
+											errorMessages={{
+												max: 'Amount cannot exceed 24',
+												min: 'Amount cannot be below 0',
+												nonDecimal: 'Amount can’t contain a decimal point',
+												nonNegative: 'Amount can’t be negative',
+											}}
+											validators={{
+												max: true,
+												min: true,
+												nonDecimal: true,
+												nonNegative: true,
+											}}
+											min={0}
+											max={24}
+											{...field}
+										/>
+									)}
+								/>
+							</FieldLabel>
+							<FieldLabel label="Minutes:">
+								<Controller
+									name={`discounts[${index}].minutes`}
+									control={control}
+									render={({ field }) => (
+										<NumberInput
+											autoFocus
+											errorMessages={{
+												max: 'Amount cannot exceed 60',
+												min: 'Amount cannot be below 0',
+												nonNegative: 'Amount can’t be negative',
+											}}
+											validators={{
+												max: true,
+												min: true,
+												nonNegative: true,
+											}}
+											min={0}
+											max={60}
+											{...field}
+										/>
+									)}
+								/>
+							</FieldLabel>
+							<FieldLabel label="Reduction:">
+								<Controller
+									name={`discounts[${index}].reduction`}
+									control={control}
+									render={({ field }) => (
+										<NumberInput
+											autoFocus
+											errorMessages={{
+												max: 'Amount cannot exceed 100',
+												min: 'Amount cannot be below 0',
+												nonDecimal: 'Amount can’t contain a decimal point',
+												nonEmpty: 'Enter amount',
+												nonNegative: 'Amount can’t be negative',
+												nonZero: 'Amount must be greater than 0'
+											}}
+											validators={{
+												max: true,
+												min: true,
+												nonDecimal: true,
+												nonNegative: true,
+											}}
+											min={0}
+											max={100}
+											{...field}
+										/>
+									)}
+								/>
 							</FieldLabel>
 
 							<Button type="button" onClick={() => remove(index)}>Remove</Button>
@@ -84,7 +150,7 @@ export const Form = () => {
 						</div>
 					);
 				})}
-				<Button type="button" onClick={() => append({ date: new Date(), reduction: 0 })}>Add another discount</Button>
+				<Button type="button" onClick={() => append({ days: 0, hours: 0, minutes: 0, reduction: 0,})}>Add another discount</Button>
 			</form>
 			<Box className="vd-flex vd-flex--justify-between vd-flex--align-center" py={8}>
 				<Button type="submit" form="discountForm" variant="do">
